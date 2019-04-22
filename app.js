5228cdf8232eb07a91a52668aafe5acc2074cf4b
Mohib <mohib.0008@gmail.com>
2019-04-22 00:03:40 -0400

    git commit -m "project still not completed, but file are updating."

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
 $("#getBtn").click(function(){
-    
+    $.get('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo')
+    .done(function(data){
+      console.log(data);
+    })
+  .fail(function(){
+      console.log("ERROR!");
+    })
+});
+
+$("#postBtn").click(function(){
+  $.post('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo')
+  .done(function(data){
+  console.log(data);
+  })
+  .fail(function(){
+  console.log("ERROR!");
+  })
+})
+
+$("#getJSONBtn").click(function(){
+  $.getJSON('https://api.nasa.gov/planetary/apod?api_key=NNKOjkoul8n1CH18TWA9gwngW1s1SmjESPjNoUFo')
+  .done(function(data){
+    console.log(data);
+  })
+  .fail(function(){
+    console.log("there is Problems!");
+  })
+
 })
